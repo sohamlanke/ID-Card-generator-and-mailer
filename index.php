99d89d9467ee246999996abdcf9307080a414473
Noice75 <sohamlanke@gmail.com>
2021-08-20 23:55:48 +0530

    commitmsg

--- index.php
+++ index.php
@@ -24,8 +24,65 @@ if ($result === FALSE) { /* Handle error */ }
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
-<body>
-  <nav class="navbar navbar-dark bg-dark">
+<body style="background-color: #E6F6F5; width: 98%" >
+  <div class="row"> 
+    <!-- navbar row -->
+    <div class="col-4 text-center">
+      <!-- search bar       -->
+      search bar here
+    </div>
+    <div class="col-4 text-center" style="font-size: 28px; font-family: Royal King" >
+      <b>CSI PCE</b>
+      
+    </div>
+    <div class="col-4 text-center">
+       something..
+    </div>
+    <br>
+    <br><br><br>
+      <!-- big heading -->
+      <div style="width: 100%; text-align: center;
+      font-size: 50px; font-family: Poppins;">
+            Get your CSI PCE <br><b>Volunteer ID</b> card immediately
+      </div>
+      <br><br><br><br><br><br><br><br>
+        <div style="text-align: center; width: 100%;">
+          <button style="background-color: black; width: 15%; color: white;" type="button" class="btn btn-lg ">Get ID Card</button>
+        </div>
+        <!-- association -->
+        <br><br><br>
+        <div style="width: 100%; text-align: center;">
+          In Association with
+        </div>
+        <!-- images here -->
+        <div style="width: 100%; text-align: center;">
+          <img src="1519864175311-removebg-preview 1.png">          
+          <img src="636246618054799851_Mahatma_International_School__New_Panvel-removebg-preview 1.png">
+        </div>
+        <!-- footer -->
+        <br><br><br><br><br><br><br>
+        <div style="width: 100%">
+          <div class="row">
+          <div class="col-4 text-center" style="font-size: 18px; font-family: Poppins;border-left: 1px solid black;"><b>Freedom in Design</b>
+            <p style="text-align: center; ">Choose from a variety of templetes,<br>or freely iterate the design to make<br> your card uniue</p>
+          </div>
+
+          <div class="col-4 text-center" style="font-size: 18px; font-family: Poppins;border-left: 1px solid black;"><b>Freedom in Design</b>
+            <p style="text-align: center; ">Choose from a variety of templetes,<br>or freely iterate the design to make<br> your card uniue</p>
+          </div>
+
+          <div class="col-4 text-center" style="font-size: 18px; font-family: Poppins;border-left: 1px solid black;"><b>Freedom in Design</b>
+            <p style="text-align: center; ">Choose from a variety of templetes,<br>or freely iterate the design to make<br> your card uniue</p>
+          </div>
+          
+        </div>
+        </div>
+    
+    
+  </div>
+  <!-- <img src="index.png" style="width: 100%;" > -->
+
+  <!-- <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">Generate Id card</a>
 </nav>
 <div class="container" style="padding: 10px;">
@@ -54,7 +111,7 @@ if ($result === FALSE) { /* Handle error */ }
     </div>
   </div>
 </div>
-</div>
+</div> -->
 </body>
 </html>
 <script type="text/javascript">
